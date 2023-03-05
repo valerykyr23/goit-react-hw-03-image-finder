0f1c4ba2450fa3bf027f6285953b454af32068ec
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2023-03-06 00:19:53 +0200

    added basic files

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,2 @@
+// import styles from './ContactList.module.css';
+
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,5 @@
+// import { React, Component } from 'react';
+// import PropTypes from 'prop-types';
+// // import { nanoid } from 'nanoid';
+// import styles from './ContactForm.module.css';
+
